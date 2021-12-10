777bf90e7b6a6d241e66ee7cded4e61ae97319d0
Sam <s-peeters@hotmail.com>
2021-12-10 13:17:03 +0100

    dist

--- src/api.js
+++ src/api.js
@@ -1,21 +1,17 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
+const serverless = require('serverless-http')
 
 const router = require('./routes');
 
 const app = express();
 
 app.use(express.json());
-app.use(router);
+app.use('/.netlify/functions/api',router);
 
-mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Successfully connected to the database!');
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is up and running on ${process.env.PORT}`);
-    })    
-  })
-  .catch((e) => console.error(`Failed to connect to database. Error: ${e}`)
-);
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true });
+
+module.exports.handler = serverless(app);
+
 
